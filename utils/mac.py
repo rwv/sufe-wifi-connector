a689ef3741eb6bd7b6e8d11b617c533fe6488aa4
seedgou <seedgou@gmail.com>
2017-11-03 16:02:31 +0800

    get mac wifi ssid

---
+++ utils/mac.py
@@ -0,0 +1,18 @@
+import objc
+
+
+def get_wifi_ssid():
+    objc.loadBundle('CoreWLAN',
+                    bundle_path='/System/Library/Frameworks/CoreWLAN.framework',
+                    module_globals=globals())
+    interfaces = []
+    for iname in CWInterface.interfaceNames():
+        interface = CWInterface.interfaceWithName_(iname)
+    interfaces.append({
+        'interface': iname,
+        'ssid': interface.ssid(),
+        'transmit_rate': interface.transmitRate(),
+        'transmit_power': interface.transmitPower(),
+        'rssi': interface.rssi()
+    })
+    return interfaces
