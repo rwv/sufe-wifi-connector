cfdea79f37bbff87f2f7c2b7331db244f0355370
seedgou <i@zczc.cz>
2017-11-09 15:42:11 +0800

    implement tel login(fail situation) there is no account for me to test login success right now

---
+++ model/tel.py
@@ -0,0 +1,49 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+HOST = 'wlan.ct10000.com'
+URL = 'http://wlan.ct10000.com'
+
+HEADERS = {
+    'Host': HOST,
+    'Pragma': 'no-cache',
+    'Cache-Control': 'no-cache',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': r'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
+    'Accept': r'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'DNT': '1',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4'
+}
+
+
+def get_params():
+    res = requests.get('http://www.example.com')
+    soup = BeautifulSoup(res.text, "html.parser")
+    index_url = URL + soup.find_all('frame')[1]['src']
+    HEADERS['Referer'] = res.url
+    index = requests.get(index_url, headers=HEADERS)
+    index_soup = BeautifulSoup(index.text, "html.parser")
+    user_dict = dict()
+    for input_element in index_soup.find_all('input'):
+        try:
+            user_dict[input_element['name']] = input_element['value']
+        except:
+            try:
+                user_dict[input_element['name']] = ''
+            except:
+                pass
+    return user_dict, index.url
+
+
+def wifi_portal_login(username, password):
+    url = URL + '/authServlet'
+    contents, ref = get_params()
+    contents['UserName'] = username
+    contents['PassWord'] = password
+    HEADERS['Referer'] = ref
+    r = requests.post(url, headers=HEADERS, data=contents)
+    if re.search('login_fail', r.text):
+        error_msg = re.findall('\n\t\t\t(.*)<br />', r.text)[0]
+        raise Exception('Login Failed', error_msg)
