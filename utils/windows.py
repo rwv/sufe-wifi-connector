d4bf9912a42eff776886793fab70ceeed951926d
seedgou <i@zczc.cz>
2017-11-16 10:46:06 +0800

    windows exception handle

--- utils/windows.py
+++ utils/windows.py
@@ -3,6 +3,9 @@ import re
 
 
 def get_wifi_ssid():
-    results = check_output(["netsh", "wlan", "show", "interfaces"], shell=True).decode('gbk')
-    ssid = re.findall('SSID\s*: (.*)\r\n', results)[0]
+    try:
+        results = check_output(["netsh", "wlan", "show", "interfaces"], shell=True).decode('gbk')
+        ssid = re.findall('SSID\s*: (.*)\r\n', results)[0]
+    except:
+        ssid = ''
     return ssid
