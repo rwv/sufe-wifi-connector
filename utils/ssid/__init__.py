c2a1cde7ca8252a4df5adcb7512d07bc3844788b
seedgou <i@zczc.cz>
2017-11-22 23:55:46 +0800

    ssid check fix

--- utils/ssid/__init__.py
+++ utils/ssid/__init__.py
@@ -1,5 +1,5 @@
 class SSID:
-    def __init__(self, os):
+    def __init__(self, os, network_type):
         if os == 'windows':
             from .windows import get_wifi_ssid
         elif os == 'mac':
@@ -7,6 +7,10 @@ class SSID:
         else:
             from .linux import get_wifi_ssid
         self.__get = get_wifi_ssid
+        self.__network_type = network_type
 
     def get(self):
         return self.__get()
+
+    def check(self):
+        return self.__get() == 'sufe-{}'.format(self.__network_type)
