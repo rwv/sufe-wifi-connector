cdcd69ed31c7dee1a9d7b73c0a9fd8ac16d7d688
seedgou <i@zczc.cz>
2017-11-30 08:44:47 +0800

    telcom

--- model/tel.py
+++ model/tel.py
@@ -1,6 +1,8 @@
+import re
+
 import requests
 from bs4 import BeautifulSoup
-import re
+
 from utils.log import logging
 
 HOST = 'wlan.ct10000.com'
@@ -17,11 +19,13 @@ HEADERS = {
     'Accept-Encoding': 'gzip, deflate',
     'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4'
 }
+session = requests.Session()
 
 
 def get_params():
+    global session
     logging.info('Get http://www.example.com')
-    res = requests.get('http://www.example.com')
+    res = session.get('http://www.example.com')
     soup = BeautifulSoup(res.text, "html.parser")
     index_url = URL + soup.find_all('frame')[1]['src']
     HEADERS['Referer'] = res.url
@@ -40,11 +44,26 @@ def get_params():
     return user_dict, index.url
 
 
-def do_logout():
+def do_logout(paramStr):
+    url = URL + "/logoutServlet"
+    contents = {
+        'paramStr': paramStr,
+        'logoutType': 'ajaxshow',
+        't': 0
+    }
+    logging.info('Get {}'.format(url))
+    r = requests.get(url, params=contents)
+    logging.debug('Contents: {}'.format(str(contents)))
+    logging.debug('Response: {}'.format(r.text))
+
+
+def do_heartbeat():
     pass
 
 
 def wifi_portal_login(username, password):
+    global session
+    session = requests.Session()
     url = URL + '/authServlet'
     contents, ref = get_params()
     contents['UserName'] = username
@@ -52,7 +71,17 @@ def wifi_portal_login(username, password):
     HEADERS['Referer'] = ref
     logging.info('Post {}'.format(url))
     logging.debug('Contents: {}'.format(str(contents)))
-    r = requests.post(url, headers=HEADERS, data=contents)
+    r = session.post(url, headers=HEADERS, data=contents)
+    soup = BeautifulSoup(r.text, "html.parser")
+    paramStr = soup.find('input', {"id": "paramStr"})['value']
     if re.search('login_fail', r.text):
         error_msg = re.findall('\n\t\t\t(.*)<br />', r.text)[0]
         raise Exception('Login Failed', error_msg)
+    else:
+        def logout():
+            do_logout(paramStr)
+
+        def heartbeat():
+            do_heartbeat()
+
+        return heartbeat, logout
